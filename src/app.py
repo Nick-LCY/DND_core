5bbe0652872f08721e3602e92789518c1649fcac
NickLin <nicklin9907@gmail.com>
2024-10-18 13:34:27 +0000

    CORS

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from data_finder import find_data_by_type, find_data_by_id
+from flask_cors import CORS
+from .data_finder import find_data_by_type, find_data_by_id
 
 app = Flask("dnd_core_api")
+cors = CORS(app, resources={r"/data/*": {"origins": ["https://dnd5e.nicklin.work"]}})
 
 @app.get("/data/<namespace>/<resource_type>")
 def get_data_by_type(namespace, resource_type):
@@ -10,3 +12,6 @@ def get_data_by_type(namespace, resource_type):
 @app.get("/data/<data_id>")
 def get_data_by_id(data_id):
     return find_data_by_id(data_id)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
